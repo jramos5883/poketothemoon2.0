e6fcecafef3b26d0dc439e6882921eedcd92a9b8
John Ramos <jramos5883@gmail.com>
2023-04-06 17:42:49 -0700

    Working up version 2 of poke to the moon, using this project to learn next 13

---
+++ pttmv2/app/page.tsx
@@ -0,0 +1,14 @@
+import "../app/globals.css";
+import "../components/navbar.component";
+import Navbar from "../components/navbar.component";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pttmv2/components/navbar.component.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      <Link href="">Home</Link>
+      <Link href="">About Us</Link>
+      <Link href="">Menu</Link>
+      <Link href="">Location</Link>
+    </div>
+  );
+};
+
+export default Navbar;
