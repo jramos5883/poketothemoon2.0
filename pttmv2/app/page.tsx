b4bb8ff3f36cd63a99aaf876c28db659c0b60368
John Ramos <jramos5883@gmail.com>
2023-04-10 14:42:33 -0700

    Finilizing some styling on home and about page.

--- pttmv2/app/page.tsx
+++ pttmv2/app/page.tsx
@@ -8,15 +8,20 @@ export default function Page() {
       <Navbar />
       <Carousel />
       <div className="home-container container-fluid">
-        <h3 className="text-indent">Delicous Poke only at Poke To The Moon!</h3>
+        <h2 className="p3">
+          Delicious Poke only at Poke To The Moon!
+        </h2>
         <h1 className="home-header">What is Poke?</h1>
         <h4 className="home-intro text-indent">
           A poke bowl is a Hawaiian dish that typically consists of marinated
           diced raw fish served over a bed of rice with various toppings such as
           vegatables, fruits, and sauces. It is a healthy and flavorful meal
           that has become popular globally for its unique taste and versatility.
-          Here at Poke to the Moon, we emphasize poke`&apos;`s versatility with
-          our boujee add ons that are unique to our poke place.
+          Here at Poke to the Moon, we emphasize poke&apos;s versatility with
+          our boujee add ons that are unique to our poke place. By combining
+          traditional flavors with innovative twists, we strive to deliver a
+          memorable culinary journey that showcases the best of what poke
+          cuisine has to offer.
         </h4>
       </div>
       <Footer />
